668229c5cba51d267728fd6f0224e0b739ffb0c3
ibachar <ibachar@e260023c-5348-0410-a714-2bcf037eb5bf>
2008-07-26 14:22:40 +0000

    git-svn-id: https://firecube.googlecode.com/svn/trunk@21 e260023c-5348-0410-a714-2bcf037eb5bf

--- FireCube/Application.h
+++ FireCube/Application.h
@@ -4,6 +4,11 @@
 #pragma warning(push)
 #pragma warning(disable:4251)
 
+/** 
+Define an application context.
+Contains pointers to various resource managers(textures,shaders).
+The context is used by various resource loading functions.
+*/
 class FIRECUBE_API ApplicationContext
 {
 public:
@@ -12,21 +17,58 @@ public:
 	TextureManager *textureManager;
 	ShaderManager *shaderManager;
 };
+/** 
+This class is reponsible for the intialization and running of the application.
+*/
 class FIRECUBE_API Application
 {
 public:
 	Application();
 	virtual ~Application();
-
+	/** 
+	Creates a default windows and intializes OpenGL.
+	@return true on success, false otherwise.
+	*/
 	bool Initialize();
+	/** 
+	Destroy the window and it's associated OpenGL context.
+	@return true on success, false otherwise.
+	*/
 	bool Destroy();
+	/** 
+	Enters the main rendering loop.
+	*/
 	void Run();
+	/** 
+	Sets the title of the window.
+	*/
 	void SetTitle(string &title);
+	/** 
+	Returns the current frames per second.
+	*/
 	float GetFps();
+	/** 
+	Sets the current application context.
+	*/
 	static void SetContext(ApplicationContext &context);
+	/** 
+	Returns the current application context.
+	*/
 	static ApplicationContext GetContext();
+	/** 
+	User should override this to handle the input.
+	Called once per frame.
+	*/
 	virtual bool HandleInput(float time) = 0;
+	/** 
+	User should override this to handle the game logic.
+	Called once per frame.
+	*/
 	virtual bool Update(float time) = 0;
+	/** 
+	User should override this to redner the game world.
+	Called once per frame.
+	*/
 	virtual bool Render(float time) = 0;
 private:
 	Timer timer;	
