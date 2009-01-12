be916dc1ac80da91dadead776fb120826887dac0
ibachar <ibachar@e260023c-5348-0410-a714-2bcf037eb5bf>
2009-01-12 12:51:47 +0000

    Shadow map experiment.
    git-svn-id: https://firecube.googlecode.com/svn/trunk@94 e260023c-5348-0410-a714-2bcf037eb5bf

--- FireCubeTest/app.h
+++ FireCubeTest/app.h
@@ -4,7 +4,14 @@ public:
 	void HandleInput(float t);
 	void Render(float t);
 	void Update(float t);
+	void SetupLight();
+	void RenderShadowMap();
+	void RenderDepth();
+	bool Init();
+	FrameBuffer fb;
 	ModelManager modelManager;
 	Model model;	
-	Font font;
+	Font font;	
+	Program plain,shadowMap,program;
+	mat4 lightProj,lightModelview;
 };
