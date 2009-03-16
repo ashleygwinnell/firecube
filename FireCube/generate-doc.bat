f0e01ad8b17b6856491527a8504038ef02000977
ibachar <ibachar@e260023c-5348-0410-a714-2bcf037eb5bf>
2009-03-16 22:46:40 +0000

    Fixed documentation.
    git-svn-id: https://firecube.googlecode.com/svn/trunk@116 e260023c-5348-0410-a714-2bcf037eb5bf

--- FireCube/generate-doc.bat
+++ FireCube/generate-doc.bat
@@ -1,5 +1,7 @@
 cppdoc_cmd.exe -title=FireCube -classdir=projects -module="cppdoc-standard" -extensions="c,cpp,cxx,cc,h,hpp,hxx" -languages="c=cpp,cc=cpp,cpp=cpp,cs=csharp,cxx=cpp,h=cpp,hpp=cpp,hxx=cpp,java=java" -overwrite "..\FireCube" #Debug #Release #.svn #glee.c #glee.h #privateFont.h ".\Documentation\index.html"
 cd Documentation
+svn add *.html
+svn propset svn:mime-type "text/html" *.html
 cd projects
 cd FireCube
 svn add *.html
