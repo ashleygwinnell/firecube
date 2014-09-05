6833f4d73ba97d3a0c5c5f74d3dbb6b2f17b00c0
Idan Bachar <ibachar@gmail.com>
2014-09-06 00:04:45 +0300

    Changed LogLevel enum to enum class

--- FireCube/Utils/Logger.cpp
+++ FireCube/Utils/Logger.cpp
@@ -6,7 +6,7 @@
 using namespace FireCube;
 
 std::ofstream file;
-Logger::LogLevel minLevel = Logger::LOG_DEBUG;
+Logger::LogLevel minLevel = Logger::LogLevel::LEVEL_DEBUG;
 bool outputToFile = true, outputToCout = true;
 
 char* levelNames[] = {"[DEBUG]", "[INFO]", "[TRACE]", "[WARNING]", "[ERROR]"};
@@ -23,10 +23,10 @@ void Logger::Write(LogLevel level, const std::string &str)
     if (outputToFile)
     {
         if (file.is_open())
-            file << levelNames[level] << ": " << str << std::endl;
+            file << levelNames[static_cast<int>(level)] << ": " << str << std::endl;
     }
     if (outputToCout)
-        std::cout << levelNames[level] << ": " << str << std::endl;
+        std::cout << levelNames[static_cast<int>(level)] << ": " << str << std::endl;
 }
 
 void FIRECUBE_API Logger::SetLogLevel(Logger::LogLevel level)
--- FireCube/Utils/Logger.h
+++ FireCube/Utils/Logger.h
@@ -7,9 +7,9 @@ namespace FireCube
 {
 namespace Logger
 {
-enum LogLevel
+enum class LogLevel
 {
-    LOG_DEBUG, LOG_INFO, LOG_TRACE, LOG_WARNING, LOG_ERROR
+	LEVEL_DEBUG, LEVEL_INFO, LEVEL_TRACE, LEVEL_WARNING, LEVEL_ERROR
 };
 void FIRECUBE_API Init(const std::string &filename);
 void FIRECUBE_API Write(LogLevel level, const std::string &str);
@@ -19,29 +19,29 @@ void FIRECUBE_API Close();
 }
 
 #ifdef ENABLE_LOGGING
-#define LOGDEBUG1(m0) { std::ostringstream oss; oss << m0; Logger::Write(Logger::LOG_DEBUG, oss.str());}
-#define LOGINFO1(m0)  { std::ostringstream oss; oss << m0; Logger::Write(Logger::LOG_INFO, oss.str());}
-#define LOGTRACE1(m0) { std::ostringstream oss; oss << m0; Logger::Write(Logger::LOG_TRACE, oss.str());}
-#define LOGWARNING1(m0) { std::ostringstream oss; oss << m0; Logger::Write(Logger::LOG_WARNING, oss.str());}
-#define LOGERROR1(m0) { std::ostringstream oss; oss << m0; Logger::Write(Logger::LOG_ERROR, oss.str());}
+#define LOGDEBUG1(m0) { std::ostringstream oss; oss << m0; Logger::Write(Logger::LogLevel::LEVEL_DEBUG, oss.str());}
+#define LOGINFO1(m0)  { std::ostringstream oss; oss << m0; Logger::Write(Logger::LogLevel::LEVEL_INFO, oss.str());}
+#define LOGTRACE1(m0) { std::ostringstream oss; oss << m0; Logger::Write(Logger::LogLevel::LEVEL_TRACE, oss.str());}
+#define LOGWARNING1(m0) { std::ostringstream oss; oss << m0; Logger::Write(Logger::LogLevel::LEVEL_WARNING, oss.str());}
+#define LOGERROR1(m0) { std::ostringstream oss; oss << m0; Logger::Write(Logger::LogLevel::LEVEL_ERROR, oss.str());}
 
-#define LOGDEBUG2(m0, m1) { std::ostringstream oss; oss << m0 << m1; Logger::Write(Logger::LOG_DEBUG, oss.str());}
-#define LOGINFO2(m0, m1) { std::ostringstream oss; oss << m0 << m1; Logger::Write(Logger::LOG_INFO, oss.str());}
-#define LOGTRACE2(m0, m1) { std::ostringstream oss; oss << m0 << m1; Logger::Write(Logger::LOG_TRACE, oss.str());}
-#define LOGWARNING2(m0, m1) { std::ostringstream oss; oss << m0 << m1; Logger::Write(Logger::LOG_WARNING, oss.str());}
-#define LOGERROR2(m0, m1) { std::ostringstream oss; oss << m0 << m1; Logger::Write(Logger::LOG_ERROR, oss.str());}
+#define LOGDEBUG2(m0, m1) { std::ostringstream oss; oss << m0 << m1; Logger::Write(Logger::LogLevel::LEVEL_DEBUG, oss.str());}
+#define LOGINFO2(m0, m1) { std::ostringstream oss; oss << m0 << m1; Logger::Write(Logger::LogLevel::LEVEL_INFO, oss.str());}
+#define LOGTRACE2(m0, m1) { std::ostringstream oss; oss << m0 << m1; Logger::Write(Logger::LogLevel::LEVEL_TRACE, oss.str());}
+#define LOGWARNING2(m0, m1) { std::ostringstream oss; oss << m0 << m1; Logger::Write(Logger::LogLevel::LEVEL_WARNING, oss.str());}
+#define LOGERROR2(m0, m1) { std::ostringstream oss; oss << m0 << m1; Logger::Write(Logger::LogLevel::LEVEL_ERROR, oss.str());}
 
-#define LOGDEBUG3(m0, m1, m2) { std::ostringstream oss; oss << m0 << m1 << m2; Logger::Write(Logger::LOG_DEBUG, oss.str());}
-#define LOGINFO3(m0, m1, m2) { std::ostringstream oss; oss << m0 << m1 << m2; Logger::Write(Logger::LOG_INFO, oss.str());}
-#define LOGTRACE3(m0, m1, m2) { std::ostringstream oss; oss << m0 << m1 << m2; Logger::Write(Logger::LOG_TRACE, oss.str());}
-#define LOGWARNING3(m0, m1, m2) { std::ostringstream oss; oss << m0 << m1 << m2; Logger::Write(Logger::LOG_WARNING, oss.str());}
-#define LOGERROR3(m0, m1, m2) { std::ostringstream oss; oss << m0 << m1 << m2; Logger::Write(Logger::LOG_ERROR, oss.str());}
+#define LOGDEBUG3(m0, m1, m2) { std::ostringstream oss; oss << m0 << m1 << m2; Logger::Write(Logger::LogLevel::LEVEL_DEBUG, oss.str());}
+#define LOGINFO3(m0, m1, m2) { std::ostringstream oss; oss << m0 << m1 << m2; Logger::Write(Logger::LogLevel::LEVEL_INFO, oss.str());}
+#define LOGTRACE3(m0, m1, m2) { std::ostringstream oss; oss << m0 << m1 << m2; Logger::Write(Logger::LogLevel::LEVEL_TRACE, oss.str());}
+#define LOGWARNING3(m0, m1, m2) { std::ostringstream oss; oss << m0 << m1 << m2; Logger::Write(Logger::LogLevel::LEVEL_WARNING, oss.str());}
+#define LOGERROR3(m0, m1, m2) { std::ostringstream oss; oss << m0 << m1 << m2; Logger::Write(Logger::LogLevel::LEVEL_ERROR, oss.str());}
 
-#define LOGDEBUG4(m0, m1, m2, m3) { std::ostringstream oss; oss << m0 << m1 << m2 << m3; Logger::Write(Logger::LOG_DEBUG, oss.str());}
-#define LOGINFO4(m0, m1, m2, m3) { std::ostringstream oss; oss << m0 << m1 << m2 << m3; Logger::Write(Logger::LOG_INFO, oss.str());}
-#define LOGTRACE4(m0, m1, m2, m3) { std::ostringstream oss; oss << m0 << m1 << m2 << m3; Logger::Write(Logger::LOG_TRACE, oss.str());}
-#define LOGWARNING4(m0, m1, m2, m3) { std::ostringstream oss; oss << m0 << m1 << m2 << m3; Logger::Write(Logger::LOG_WARNING, oss.str());}
-#define LOGERROR4(m0, m1, m2, m3) { std::ostringstream oss; oss << m0 << m1 << m2 << m3; Logger::Write(Logger::LOG_ERROR, oss.str());}
+#define LOGDEBUG4(m0, m1, m2, m3) { std::ostringstream oss; oss << m0 << m1 << m2 << m3; Logger::Write(Logger::LogLevel::LEVEL_DEBUG, oss.str());}
+#define LOGINFO4(m0, m1, m2, m3) { std::ostringstream oss; oss << m0 << m1 << m2 << m3; Logger::Write(Logger::LogLevel::LEVEL_INFO, oss.str());}
+#define LOGTRACE4(m0, m1, m2, m3) { std::ostringstream oss; oss << m0 << m1 << m2 << m3; Logger::Write(Logger::LogLevel::LEVEL_TRACE, oss.str());}
+#define LOGWARNING4(m0, m1, m2, m3) { std::ostringstream oss; oss << m0 << m1 << m2 << m3; Logger::Write(Logger::LogLevel::LEVEL_WARNING, oss.str());}
+#define LOGERROR4(m0, m1, m2, m3) { std::ostringstream oss; oss << m0 << m1 << m2 << m3; Logger::Write(Logger::LogLevel::LEVEL_ERROR, oss.str());}
 
 
 #define COUNT_LOG_ARGS_IMPL2(_1, _2, _3, _4, count, ...) count
