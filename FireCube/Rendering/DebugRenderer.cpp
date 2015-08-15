c562adc67e5a6b41b489c17985e47c1bc6cb10a7
Idan Bachar <ibachar@gmail.com>
2015-08-15 12:23:38 +0300

    Added DebugRenderer::AddCapsule

--- FireCube/Rendering/DebugRenderer.cpp
+++ FireCube/Rendering/DebugRenderer.cpp
@@ -100,3 +100,90 @@ void DebugRenderer::AddSphere(vec3 center, float radius, unsigned int rings, uns
 		}
 	}	
 }
+
+void DebugRenderer::AddCapsule(vec3 p0, vec3 p1, float radius, unsigned int rings, unsigned int columns, const vec3 &color)
+{
+	const float fDeltaRingAngle = (float)PI * 0.5f / rings;
+	const float fDeltacolumnAngle = 2 * (float)PI / columns;
+
+	vec3 up = (p1 - p0).Normalized();
+	vec3 right, z;
+	if (fabs(up.Dot(vec3(0, 1, 0))) > 1 - 0.0001f)
+	{
+		right = vec3(1, 0, 0);
+		z = vec3(0, 0, 1);
+	}
+	else
+	{
+		z = Cross(up, vec3(0, 1, 0));
+		right = Cross(up, z);
+		z.Normalize();
+		right.Normalize();
+	}
+
+	mat4 transformation = mat4::IDENTITY;
+	transformation.m[0] = right.x;
+	transformation.m[4] = right.y;
+	transformation.m[8] = right.z;
+
+	transformation.m[1] = up.x;
+	transformation.m[5] = up.y;
+	transformation.m[9] = up.z;
+
+	transformation.m[2] = z.x;
+	transformation.m[6] = z.y;
+	transformation.m[10] = z.z;
+
+	for (unsigned int ring = 0; ring < rings; ring++)
+	{
+		const float r0 = std::sin(ring * fDeltaRingAngle);
+		const float y0 = std::cos(ring * fDeltaRingAngle);
+		const float r1 = std::sin((ring + 1) * fDeltaRingAngle);
+		const float y1 = std::cos((ring + 1) * fDeltaRingAngle);
+
+		for (unsigned column = 0; column < columns; column++)
+		{
+			vec3 pos00(-r0 * std::sin(column * fDeltacolumnAngle), y0, -r0 * std::cos(column * fDeltacolumnAngle));
+			vec3 pos10(-r0 * std::sin((column + 1) * fDeltacolumnAngle), y0, -r0 * std::cos((column + 1) * fDeltacolumnAngle));
+			vec3 pos01(-r1 * std::sin(column * fDeltacolumnAngle), y1, -r1 * std::cos(column * fDeltacolumnAngle));
+			vec3 pos11(-r1 * std::sin((column + 1) * fDeltacolumnAngle), y1, -r1 * std::cos((column + 1) * fDeltacolumnAngle));
+			pos00.Normalize();
+			pos10.Normalize();
+			pos01.Normalize();
+			pos11.Normalize();
+			AddLine(p1 + transformation * pos00 * radius, p1 + transformation * pos10 * radius, color);
+			AddLine(p1 + transformation * pos10 * radius, p1 + transformation * pos11 * radius, color);
+			AddLine(p1 + transformation * pos11 * radius, p1 + transformation * pos01 * radius, color);
+			AddLine(p1 + transformation * pos01 * radius, p1 + transformation * pos00 * radius, color);
+
+			pos00.y *= -1;
+			pos01.y *= -1;
+			pos10.y *= -1;
+			pos11.y *= -1;
+
+			AddLine(p0 + transformation * pos00 * radius, p0 + transformation * pos10 * radius, color);
+			AddLine(p0 + transformation * pos10 * radius, p0 + transformation * pos11 * radius, color);
+			AddLine(p0 + transformation * pos11 * radius, p0 + transformation * pos01 * radius, color);
+			AddLine(p0 + transformation * pos01 * radius, p0 + transformation * pos00 * radius, color);
+		}
+	}
+
+
+	for (unsigned column = 0; column < columns; column++)
+	{
+		vec3 pos00(-1 * std::sin(column * fDeltacolumnAngle), 0, -1 * std::cos(column * fDeltacolumnAngle));
+		vec3 pos10(-1 * std::sin((column + 1) * fDeltacolumnAngle), 0, -1 * std::cos((column + 1) * fDeltacolumnAngle));		
+		pos00.Normalize();
+		pos10.Normalize();		
+
+		vec3 pos00p0 = pos00;
+		vec3 pos10p0 = pos10;
+		pos00p0.y *= -1;
+		pos10p0.y *= -1;
+
+		AddLine(p1 + transformation * pos00 * radius, p1 + transformation * pos10 * radius, color);
+		AddLine(p1 + transformation * pos10 * radius, p0 + transformation * pos10p0 * radius, color);
+		AddLine(p0 + transformation * pos10p0 * radius, p0 + transformation * pos00p0 * radius, color);
+		AddLine(p0 + transformation * pos00p0 * radius, p1 + transformation * pos00 * radius, color);
+	}
+}
--- FireCube/Rendering/DebugRenderer.h
+++ FireCube/Rendering/DebugRenderer.h
@@ -23,6 +23,7 @@ public:
 	void AddLine(const vec3 &start, const vec3 &end, const vec3 &color);
 	void AddBoundingBox(const BoundingBox &boundingBox, const vec3 &color);	
 	void AddSphere(vec3 center, float radius, unsigned int rings, unsigned int columns, const vec3 &color);
+	void AddCapsule(vec3 p0, vec3 p1, float radius, unsigned int rings, unsigned int columns, const vec3 &color);
 private:
 	struct LineVertex
 	{
