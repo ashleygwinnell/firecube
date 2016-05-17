3c899ca588d2416b38b8eeee5a17fba69cbb71c6
Idan Bachar <ibachar@gmail.com>
2016-05-17 20:08:47 +0300

    Added simple event system for Lua scripts

--- FireCube/Scripts/core.lua
+++ FireCube/Scripts/core.lua
@@ -4,3 +4,49 @@ function Script()
 		SubscribeToEvent = function(self, ...) self.script:SubscribeToEvent(...) end 
 	} 
 end
+
+EventManager = {
+	events = {}
+}
+	
+function EventManager:SubscribeToEvent(event, object, callback)
+	local evt = self:_GetEvent(event)
+	
+	local found = false
+	for index, value in ipairs (evt.listeners) do
+		if value[1] == object then
+			found = true
+			break
+		end
+	end
+	
+	if not found then
+		table.insert(evt.listeners, {object, callback})		
+	end
+end
+
+function EventManager:EmitEvent(event, ...)	
+	local evt = self:_GetEvent(event)
+		
+	for index, value in ipairs (evt.listeners) do
+		local obj = value[1]
+		local func = value[2]
+		
+		func(obj, ...)		
+	end
+end
+
+function EventManager:_GetEvent(event)
+	local ret = self.events[event]
+	
+	if not ret then
+		ret = {
+			name = event,
+			listeners = {}
+		}
+		
+		self.events[event] = ret
+	end
+	
+	return ret
+end
