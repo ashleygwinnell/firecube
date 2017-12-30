d67dc41fcb8aebff04271c2f8e9153923fd30726
Idan Bachar <ibachar@gmail.com>
2017-12-30 12:25:51 +0200

    SceneEditor: Display contents of assets folder

--- Tools/SceneEditor/AssetBrowserWindow.cpp
+++ Tools/SceneEditor/AssetBrowserWindow.cpp
@@ -15,13 +15,110 @@ void AssetBrowserWindow::Render()
 
 	ImGui::SetNextDock(ImGuiDockSlot_Bottom);
 	if (ImGui::BeginDock("Asset Browser", nullptr, 0, ImVec2(50, -1)))
-	{		
+	{
+		ImGui::Columns(3, "assetBrowserColumns", true);
+		ImGui::Separator();
+
+		auto assetsFolder = Filesystem::GetAssetsFolder();
+		if (assetsFolder.empty() == false)
+		{
+			RenderDirectoryTree(assetsFolder);
+		}
+		ImGui::NextColumn();
+		if (filesInSelectedPath.empty() == false)
+		{
+			for (auto &file : filesInSelectedPath)
+			{
+				std::string label = Filesystem::GetLastPathComponent(file);
+				if (ImGui::Selectable(label.c_str())) 
+				{
+
+				}
+			}
+		}
 	}
 	ImGui::EndDock();
+}
+
+void AssetBrowserWindow::RenderDirectoryTree(const std::string &path)
+{
+	ImGuiTreeNodeFlags nodeFlags = ImGuiTreeNodeFlags_OpenOnArrow | ImGuiTreeNodeFlags_OpenOnDoubleClick | (selectedPath == path ? ImGuiTreeNodeFlags_Selected : 0);
+	std::vector<std::string> items = GetDirectoriesInPath(path);
+	if (items.empty())
+	{
+		nodeFlags |= ImGuiTreeNodeFlags_Leaf | ImGuiTreeNodeFlags_NoTreePushOnOpen;
+	}
+	ImGui::PushID(path.c_str());
+	std::string label = path == Filesystem::GetAssetsFolder() ? "Assets" : Filesystem::GetLastPathComponent(path);
+	bool nodeOpen = ImGui::TreeNodeEx(label.c_str(), nodeFlags);
+	ImGui::PopID();
+	if (ImGui::IsItemClicked())
+	{
+		selectedPath = path;
+		filesInSelectedPath = GetFilesInPath(path);
+	}
+
+	if (nodeOpen && items.empty() == false)
+	{
+		for (auto &p : items)
+		{
+			RenderDirectoryTree(p);
+		}
+		ImGui::TreePop();
+	}	
 }
 
 void AssetBrowserWindow::SetScene(NodeDescriptor *rootDesc, EditorState *editorState)
 {
 	this->rootDesc = rootDesc;
 	this->editorState = editorState;	
+}
+
+std::vector<std::string> AssetBrowserWindow::GetDirectoriesInPath(const std::string &path)
+{
+	std::vector<std::string> ret;
+	std::string pattern(path);
+	pattern.append("\\*");
+	WIN32_FIND_DATAA data;
+	HANDLE hFind;
+	if ((hFind = FindFirstFileA(pattern.c_str(), &data)) != INVALID_HANDLE_VALUE)
+	{
+		do
+		{
+			if (data.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+			{
+				std::string p = data.cFileName;
+				if (p != "." && p != "..")
+				{
+					ret.push_back(Filesystem::JoinPath(path, p));
+				}
+			}
+		} while (FindNextFileA(hFind, &data) != 0);
+		FindClose(hFind);
+	}
+
+	return ret;
+}
+
+std::vector<std::string> AssetBrowserWindow::GetFilesInPath(const std::string &path)
+{
+	std::vector<std::string> ret;
+	std::string pattern(path);
+	pattern.append("\\*");
+	WIN32_FIND_DATAA data;
+	HANDLE hFind;
+	if ((hFind = FindFirstFileA(pattern.c_str(), &data)) != INVALID_HANDLE_VALUE)
+	{
+		do
+		{
+			if ((data.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) == 0)
+			{
+				std::string p = data.cFileName;
+				ret.push_back(Filesystem::JoinPath(path, p));
+			}
+		} while (FindNextFileA(hFind, &data) != 0);
+		FindClose(hFind);
+	}
+
+	return ret;
 }
--- Tools/SceneEditor/AssetBrowserWindow.h
+++ Tools/SceneEditor/AssetBrowserWindow.h
@@ -13,7 +13,14 @@ public:
 	AssetBrowserWindow(FireCube::Engine *engine);
 	void Render();
 	void SetScene(NodeDescriptor *rootDesc, EditorState *editorState);
+	void RenderDirectoryTree(const std::string &path);
 private:
+	std::vector<std::string> GetDirectoriesInPath(const std::string &path);
+	std::vector<std::string> GetFilesInPath(const std::string &path);
+
 	NodeDescriptor *rootDesc;
 	EditorState *editorState;	
+
+	std::string selectedPath;
+	std::vector<std::string> filesInSelectedPath;
 };
