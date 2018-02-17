0442d60ba3388bf3945c7494c5eb70f951cd674a
ibachar <ibachar@gmail.com>
2018-02-17 21:28:28 +0200

    SceneEditor: Added support for dropping amesh from asset browser to static model

--- Tools/SceneEditor/StaticModelWindow.cpp
+++ Tools/SceneEditor/StaticModelWindow.cpp
@@ -16,6 +16,7 @@ void StaticModelWindow::Render(EditorState *editorState, StaticModelDescriptor *
 {
 	std::string meshFileName = descriptor->GetMeshFilename();
 	std::string selectedPath;
+	ImGui::BeginGroup();
 	ImGui::InputText("", &meshFileName[0], meshFileName.size() + 1, ImGuiInputTextFlags_ReadOnly);
 	ImGui::SameLine();
 	if (ImGui::Button("...##meshOpenButton"))
@@ -24,6 +25,39 @@ void StaticModelWindow::Render(EditorState *editorState, StaticModelDescriptor *
 	}
 	ImGui::SameLine();
 	ImGui::Text("Mesh");
+	ImGui::EndGroup();
+	if (ImGui::BeginDragDropTarget())
+	{
+		const ImGuiPayload *payload = ImGui::AcceptDragDropPayload("asset");
+		if (payload)
+		{
+			AssetType type;
+			std::string path;
+			AssetUtils::DeserializeAssetDescription((const char *)payload->Data, type, path);
+			std::replace(path.begin(), path.end(), '\\', '/');
+			path = Filesystem::MakeRelativeTo(Filesystem::GetAssetsFolder(), path);
+
+			if (type == AssetType::MESH)
+			{
+				std::string oldMeshFileName = descriptor->GetMeshFilename();
+				auto command = new CustomCommand(editorState, "Change Mesh", [descriptor, path, this]()
+				{
+					descriptor->SetMeshFilename(path, engine);
+					descriptor->componentChanged(nullptr);
+				}, [descriptor, oldMeshFileName, this]()
+				{
+					if (oldMeshFileName.empty() == false)
+					{
+						descriptor->SetMeshFilename(oldMeshFileName, engine);
+						descriptor->componentChanged(nullptr);
+					}
+				});
+
+				editorState->ExecuteCommand(command);
+			}
+		}
+		ImGui::EndDragDropTarget();
+	}
 	if (ImGuiHelpers::AssetSelectionPopup("Select Mesh", Filesystem::JoinPath(Filesystem::GetAssetsFolder(), "Models"), selectedPath))
 	{
 		selectedPath = Filesystem::MakeRelativeTo(Filesystem::GetAssetsFolder(), selectedPath);
